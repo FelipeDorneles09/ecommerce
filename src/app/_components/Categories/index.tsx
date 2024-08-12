ab083c5e7f91393d25f83b7c2a255a02be59624b
Felipe <fdorneles63@gmail.com>
2024-08-12 19:48:54 -0300

    adsadawdq

--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Link from 'next/link'
+
 
 import { Category } from '../../../payload/payload-types'
 import CategoryCard from './CategoryCard'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
 
 const Categories = ({ categories }: { categories: Category[] }) => {
   return (
