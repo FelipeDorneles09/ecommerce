1a0de1bcbc8904243364db4611809ed5b2548e71
Felipe <fdorneles63@gmail.com>
2024-08-12 19:52:39 -0300

    adasdqawfa

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,18 @@ const { rules } = require('eslint-config-prettier')
 
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
+  extends: [
+    'plugin:@next/next/recommended',
+    '@payloadcms',
+    'plugin:simple-import-sort/recommended'
+  ],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'simple-import-sort'],
   rules: {
     'prettier/prettier': 'off',  // Desabilita o Prettier temporariamente
     'no-console': 'off',
     'eol-last': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
-  
 }
-
